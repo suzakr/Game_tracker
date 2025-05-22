5679ffc4bf4aaa962a8f14264370e3233a610607
Martin Fedorcak <martin.fedorcak44@gmail.com>
2025-05-22 20:10:23 +0200

    This is Game tracker

--- game_tracker/Game.py
+++ game_tracker/Game.py
@@ -3,11 +3,11 @@ class Game:
     def __init__(self) -> None:
         self.game_dict = {}
 
-    def get_game_title(self) -> None:
+    def add_game_title(self) -> None:
         x = input("You can choose or add new game:")
         self.game_dict["Game"] = x
 
-    def get_playtime(self) -> dict:
+    def add_playtime(self) -> dict:
         while True:
             y = input("Add time (in hours):")
             try:
@@ -17,17 +17,3 @@ class Game:
                 print("Please enter a number.")
 
         return self.game_dict
-
-
-
-# _______________________________________________________________________________________ #
-        # def add_game_time_to_json(self):
-        #     # Adds time for game (for example of using of try/excuse function)
-        #
-        #     try:
-        #         game_list["game_time"] = int(game_time)
-        #         # .pop to json?? Aby se to odstranilo z dict po exportu
-        #     except Exception as exc:
-        #         print("Not a number! Error:", exc)
-        #         del game_list["game"]
-        #         del game_list["game_time"]
--- game_tracker/main.py
+++ game_tracker/main.py
@@ -6,22 +6,42 @@ game_list = []
 while True:
     print("1. Add game")
     print("2. View stats")
-    print("3. Exit")
+    print("3. Save list")
+    print("4. Exit")
 
     choice = input("Enter your choice:")
 
     if choice == "1":
+        game_info = game_tracker.SteamAPI.get_game_info()
 
-        game_tracker.SteamAPI.get_gname()
+        game_tracker.SteamAPI.get_game_name()
         game = game_tracker.Game.Game()
-        game.get_game_title()
-        res = game.get_playtime()
-        game_list.append(res)
-    
+        game.add_game_title()
+        entered_game = game.game_dict["Game"]
+
+        match = None
+        for i in game_info:
+            if i["Game"] == entered_game:
+                match = i
+                break
+
+        if match:
+            print("Playtime added automatically.")
+            game.game_dict["Playtime"] = match["Playtime"]
+
+        else:
+            game.add_playtime()
+
+        game_list.append(game.game_dict)
+
+
     elif choice == "2":
         print(game_list)
-    
+
     elif choice == "3":
+        ...
+
+    elif choice == "4":
         print("Exiting")
         break
 
